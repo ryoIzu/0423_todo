5deee30b2b76bca978f61404cc1327963cc4709c
ryo izuta <r.izuta@mplpl.com>
2024-05-28 11:50:02 +0900

    ✨ first commitです！

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# ReactでToDoリストの作成  
+絶賛訓練中  
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/Components/StateToDo.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import '../Styles/StateTodo.css';
+
+let maxId = 0;
+
+export default function StateToDo() {
+  const [title, setTitle] = useState();
+  const [todo, setToDo] = useState([]);
+
+  const handleChangeTitle = e => {
+    setTitle(e.target.value);
+  };
+
+  const handleClick = () => {
+    setToDo([
+      ...todo,
+      {
+        id: ++maxId,            //id値
+        title,                  //ToD本体
+        created: new Date(),    //作成日時
+        isDone: false           //実行済か？否か
+      }
+    ]);
+  };
+
+  const handleDone = e => {
+    setToDo(todo.map(item => {
+      if(item.id === Number(e.target.dataset.id)){
+        return {
+          ...item,
+          isDone: true
+        };
+      }
+      else {
+        return item;
+      }
+    }));
+  };
+
+  return(
+    <div>
+      <label>
+        やること：
+        <input 
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChangeTitle} />
+      </label>
+      <button type="button" onClick={handleClick}>追加</button>
+      <hr />
+      <ul>
+        {todo.map(item=>(
+          <li 
+            key={item.id}
+            className={item.isDone ? 'done' : ''}
+          >
+            {item.title}
+            <button type="button" onClick={handleDone} data-id={item.id}>済
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
